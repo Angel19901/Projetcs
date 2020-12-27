03d79939b0437948df9ab41792b1872648effcc8
Angel1990 <316218870@pcpuma.acatlan.unam.mx>
2020-12-27 17:11:59 -0600

    User Terminado v2

--- User.py
+++ User.py
@@ -1,22 +1,24 @@
 
 from io import open;
+import os;
 
 class User:
 
     def __init__(self):
+        pass;
 
     def __FirstTime(self, TypeOfUser):
 
         TypeOfUser = str(TypeOfUser);
 
         if TypeOfUser == "root":
-            File = open("usr/root/root.dat", "r");
+            File = open("usr\\root\\root.dat", "r");
         else:
             if TypeOfUser == "admin":
-                File = open("usr/admin/admin.dat", "r");
+                File = open("usr\\admin\\admin.dat", "r");
             else:
                 if TypeOfUser == "assistant":
-                    File = open("usr/assistant/assistant.dat", "r");
+                    File = open("usr\\assistant\\assistant.dat", "r");
                 else:
                     return 3;  # Error: Usuario no encontrado.
 
@@ -42,7 +44,7 @@ class User:
 
             if TypeOfUser == "root":
 
-                File = open("usr/root/root.dat", "w");
+                File = open("usr\\root\\root.dat", "w");
                 File.write("fhbshzwxecrvtbvecwxdszddfghgzadsfdgcfb asdjnlakyutkhujtuknybjnjyukylmunikiynimuibtjdb");
                 File.close();
                 self.CreateUser(self, Name, Password, TypeOfUser);
@@ -50,7 +52,7 @@ class User:
             else:
                 if TypeOfUser == "admin":
 
-                    File = open("usr/admin/admin.dat", "w");
+                    File = open("usr\\admin\\admin.dat", "w");
                     File.write("fhbshgsdgeargerqgererwgerwgwregwrgerwgerwgrefb asdjnlafvabgxnhdcbgsbgvkjdb");
                     File.close();
                     self.CreateUser(self, Name, Password, TypeOfUser);
@@ -58,25 +60,25 @@ class User:
                 else:
                     if TypeOfUser == "assistant":
 
-                        File = open("usr/assistant/assistant.dat", "w");
+                        File = open("usr\\assistant\\assistant.dat", "w");
                         File.write("fasfeffqwefweferferferrgaerghbshfb asdvxatbwdeyuqegfywqcgfoywmerjnlakjdb");
                         File.close();
                         self.CreateUser(self, Name, Password, TypeOfUser);
 
         else:
             if TypeOfUser == "assistant":
-                File = open("usr/assistant/" + Name + "assistant.bin","wb");
+                File = open("usr\\assistant\\" + Name + "assistant.bin","wb");
                 File.write(str(Name + " " + Password + "\n"));
                 File.close();
 
             else:
                 if TypeOfUser == "root":
-                    File = open("usr/root/" + Name + "root.bin","wb");
+                    File = open("usr\\root\\" + Name + "root.bin","wb");
                     File.write(str(Name + " " + Password + "\n"));
                     File.close();
                 else:
                     if TypeOfUser == "admin":
-                        File = open("usr/admin/" + Name + "admin.bin","wb");
+                        File = open("usr\\admin\\" + Name + "admin.bin","wb");
                         File.write(str(Name + " " + Password + "\n"));
                         File.close();
 
@@ -92,12 +94,12 @@ class User:
 
             try:
 
-                File = open("usr/assistant/" + Name + "assistant.bin", "rb");
+                File = open("usr\\assistant\\" + Name + "assistant.bin", "rb");
                 Line = File.readline().split();
 
                 if Line[0] == Name and Line[1] == OldPassword:
                     File.close()
-                    File = open("usr/assistant/" + Name + "assistant.bin", "wb");
+                    File = open("usr\\assistant\\" + Name + "assistant.bin", "wb");
                     File.write(str(Name + " " + NewPassword + "\n"));
 
                 File.close();
@@ -114,13 +116,13 @@ class User:
 
                 try:
 
-                    File = open("usr/root/" + Name + "root.bin", "rb");
+                    File = open("usr\\root\\" + Name + "root.bin", "rb");
 
                     Line = File.readline().split();
 
                     if Line[0] == Name and Line[1] == OldPassword:
                         File.close();
-                        File = open("usr/root/" + Name + "root.bin", "wb");
+                        File = open("usr\\root\\" + Name + "root.bin", "wb");
                         File.write(str(Name + " " + NewPassword + "\n"));
 
                     File.close();
@@ -134,12 +136,12 @@ class User:
                 try:
 
                     if TypeOfUser == "admin":
-                        File = open("usr/admin/" + Name + "admin.bin", "wb");
+                        File = open("usr\\admin\\" + Name + "admin.bin", "wb");
                         Line = File.readline().split();
 
                         if Line[0] == Name and Line[1] == OldPassword:
                             File.close();
-                            File = open("usr/admin/" + Name + "admin.bin", "wb");
+                            File = open("usr\\admin\\" + Name + "admin.bin", "wb");
                             File.write(str(Name + " " + NewPassword + "\n"));
 
                         File.close();
@@ -148,3 +150,50 @@ class User:
 
                     File.close();
                     return "Error: No existe el usuario.";
+
+
+    def ValidUser(self, Name, Password, TypeOfUser):
+        if TypeOfUser == "root":
+            if os.path.isfile("usr\\root\\" + Name + "root.bin"):
+                File = open("usr\\root\\" + Name + "root.bin","rb");
+                Line = File.readline().split();
+
+                if Line[0] == Name and Line[1] == Password:
+                    return True;
+                else:
+                    return False;
+
+            else:
+                return False;
+
+        else:
+            if TypeOfUser == "admin":
+                if os.path.isfile("usr\\admin\\" + Name + "admin.bin"):
+                    File = open("usr\\admin\\" + Name + "admin.bin", "rb");
+                    Line = File.readline().split();
+
+                    if Line[0] == Name and Line[1] == Password:
+                        return True;
+                    else:
+                        return False;
+
+                else:
+                    return False;
+
+            else:
+                if TypeOfUser == "assistant":
+                    if os.path.isfile("usr\\assistant\\" + Name + "assistant.bin"):
+                        File = open("usr\\assistant\\" + Name + "assistant.bin", "rb");
+                        Line = File.readline().split();
+
+                        if Line[0] == Name and Line[1] == Password:
+                            return True;
+                        else:
+                            return False;
+
+                    else:
+                        return False;
+
+                else:
+
+                    return False;
