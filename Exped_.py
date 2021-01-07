893dd3184b44c5900a46c70ea18c404ac71bff55
Angel1990 <316218870@pcpuma.acatlan.unam.mx>
2021-01-07 07:17:50 -0600

    Exp avance

--- Exped_.py
+++ Exped_.py
@@ -11,7 +11,7 @@ class HISCG:
         TIPO_DE_INTERROGATORIO = ""
 
         # I. FICHA DE  IDENTIFICACION
-        NONBRE_DEL_PACIENTE = "" #(APELLIDO PATERNO, APELLIDO MATERNO Y NOMBRE (S))
+        NOMBRE_DEL_PACIENTE = "" #(APELLIDO PATERNO, APELLIDO MATERNO Y NOMBRE (S))
         EDAD = ""
         FECHA_DE_NACIMIENTO = ""
         OCUPACION_DEL_PACIENTE = ""
@@ -82,7 +82,7 @@ class HISCG:
 
         #Metodos para establcer Valores
 
-        #Seccion 0;
+        #Seccion 0
 
         def SetUNIDAD_MEDICA(self,UnidadMedica):
                 self.UNIDAD_MEDICA = UnidadMedica;
@@ -98,3 +98,114 @@ class HISCG:
 
         def SetTIPO_DE_INTERROGATORIO(self,TipoDeInterrogatorio):
                 self.TIPO_DE_INTERROGATORIO = TipoDeInterrogatorio;
+
+        #Seccion  I
+
+        def SetNOMBRE_DEL_PACIENTE(self,NombreDelPaciente): #(APELLIDO PATERNO, APELLIDO MATERNO Y NOMBRE (S))
+                self.NONBRE_DEL_PACIENTE = NombreDelPaciente;
+
+        def SetEDAD(self,Edad):
+                self.EDAD = Edad;
+
+        def SetFECHA_DE_NACIMIENTO(self,FechaDeNacimiento):
+                self.FECHA_DE_NACIMIENTO = FechaDeNacimiento
+
+        def SetOCUPACION_DEL_PACIENTE(self,OcupacionDelPaciente):
+                self.OCUPACION_DEL_PACIENTE = OcupacionDelPaciente;
+
+        def SetDOMICILIO(self, Domicilio):
+                self.DOMICILIO = Domicilio;
+
+        def SetTELEFONO(self,Telefono):
+                self.TELEFONO = Telefono;
+
+        def SetNOMBRE_DEL_PADRE_O_TUTOR(self,NombreDelPadreOTutor):
+                self.NOMBRE_DEL_PADRE_O_TUTOR = NombreDelPadreOTutor;
+
+        def SetPARENTESCO_CON_EL_PACIENTE(self,ParentescoConELPaciente):
+                self.PARENTESCO_CON_EL_PACIENTE = ParentescoConELPaciente;
+
+        # II. ANTECEDENTES HEREDOFAMILIARES
+
+        def SetAntHer(self,Antecedentes):
+                self.AntHer = Antecedentes
+
+        # III. ANTECEDENTES PERSONALES  NO PATOLOGICOS
+        def SetAntPerNoPat(self,AntPerNoPat):
+                self.AntPerNoPat = AntPerNoPat;
+
+        # IV. ANTECENTES PERSONALES PATOLOGICOS
+        def SetAntPerPat(self, AntPerPat):
+                self.AntPerPat = AntPerPat;
+
+        # V. ANTECEDENTES GINECOLOGICOS
+        def SetAntGin(self,AnteGin):
+                self.AntGin = AnteGin;
+
+        # VI. PADECIMIENTO ACTUAL
+        def SetPadAct(self,PadActual):
+                self.PadAct = PadActual
+
+        # VII. INTERROGATORIO POR APARATOS  Y SISTEMAS
+        def SetCARDIOVASCULAR(self, CarVas):
+                self.CARDIOVASCULAR = CarVas;
+
+        def SetRESPIRATORIO(self,Resp):
+                self.RESPIRATORIO = Resp
+
+        def SetGASTROINTESTINAL(self, Gas):
+                self.GASTROINTESTINAL = Gas;
+
+        def SetHEMATICO_Y_LINFATICO(self,HemLin):
+                self.HEMATICO_Y_LINFATICO = HemLin;
+
+        def SetENDOCRINO(self, Endocrino):
+                self.ENDOCRINO = Endocrino;
+
+        #VII. INTERROGATORIO POR APARATOS Y SISTEMAS
+        def SetNERVIOSO(self,Ner):
+                self.NERVIOSO = Ner;
+
+        def SetMUSCULOESQUELETICO(self,Mus):
+                self.MUSCULOESQUELETICO =  Mus;
+
+        def SetPIEL_MUCOSAS_Y_ANEXOS(self,PiMuAne):
+                self.PIEL_MUCOSAS_Y_ANEXOS = PiMuAne;
+
+        # VIII. SIGNOS VITALES
+        def SetTENSION_ARTERIAL(self,TenArt):
+                self.TENSION_ARTERIAL = TenArt;
+
+        def SetTEMPERATURA(self,Tem):
+                self.TEMPERATURA = Tem;
+
+        def SetFRECUENCIA_CARDIACA(self,FreCar):
+                self.FRECUENCIA_CARDIACA = FreCar;
+
+        def SetFRECUENCIA_RESPIRATORIA(self):
+        def SetPESO(self):
+        def SetTALLA(self):
+
+        # IX EXPLORACION FISICA
+        HABITUS_EXTERIOR = ""
+        CABEZA = ""
+        CUELLO = ""
+        TORAX = ""
+        ABDOMEN = ""
+
+        GENITALES = ""
+        EXTREMIDADES = ""
+        PIEL = ""
+        # X. RESULTADOS PREVIOS  Y ACTUALES DE LABORATORIO, GABINETE Y OTROS:
+        ResPreActLabGabOtr = ""
+
+        # XI. DIAGNOSTICOS O PROBLEMAS  CLINICOS:
+        DiaProCli = ""
+
+        # XII. TRATAMIENTO FARMACOLOGICO\
+        TraFar = ""
+        TERAPEUTICA_EMPLEADA_Y_RESULTADOS_PREVIOS = ""
+        TERAPEUTICA_ACTUAL = ""
+
+        # XIII. PRONOSTICO
+        Pro = ""
